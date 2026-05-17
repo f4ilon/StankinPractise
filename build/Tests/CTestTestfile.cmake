25cd69f2c038eb814713ee32c45bb7381eb68abc
y4r0s <conkurentik2@gmail.com>
2026-05-17 20:38:33 +0300

    fix(git): remove build directory from tracking

--- build/Tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/StankinPractice/StankinPractise/Tests
-# Build directory: D:/StankinPractice/StankinPractise/build/Tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[AllTests]=] "D:/StankinPractice/StankinPractise/build/Tests/messenger_tests.exe")
-set_tests_properties([=[AllTests]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/StankinPractice/StankinPractise/Tests/CMakeLists.txt;19;add_test;D:/StankinPractice/StankinPractise/Tests/CMakeLists.txt;0;")
